5b5f1b2e56ad61389e605d6ccc254280e4f8d5d8
Muhammad Taufik <muhammad.taufik@adira.co.id>
2024-09-05 10:47:12 +0700

    update code

--- api/v1/master-organitation/index.js
+++ api/v1/master-organitation/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const controller = require('./controller');
 const utils = require('../../../utils/utils');
 
-router.post('/config', utils.verifyTokenMs, controller.createConfig);
+router.post('/create-config', utils.verifyTokenMs, controller.createConfig);
 router.post('/remove-config', utils.verifyTokenMs, controller.removeConfig);
 router.post('/statued-config', utils.verifyTokenMs, controller.statusApproveReject);
 router.post('/config', utils.verifyTokenMs, controller.getConfig);
