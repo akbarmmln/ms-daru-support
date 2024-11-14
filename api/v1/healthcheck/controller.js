d3be8e7439caae4b199a6f13eecba35e731674d9
Muhammad Taufik <muhammad.taufik@adira.co.id>
2024-11-14 17:17:00 +0700

    update code

--- api/v1/healthcheck/controller.js
+++ api/v1/healthcheck/controller.js
@@ -4,8 +4,21 @@ const sequelize = require('../../../config/db').Sequelize;
 const logger = require('../../../config/logger');
 const errMsg = require('../../../error/resError');
 const rsMsg = require('../../../response/rs');
+const s3 = require('../../../config/oss').client;
 let health = { serverOk: false, dbOk: false };
 
+async function fileTypeFromBuffer(buffer) {
+    const { fileTypeFromBuffer } = await import('file-type')
+    return await fileTypeFromBuffer(buffer)
+}
+
+async function runNanoID(n) {
+    const { customAlphabet } = await import('nanoid');
+    const alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-';
+    const id = customAlphabet(alphabet, n);
+    return id();
+}
+  
 exports.healtyCheck = async function (req, res) {
     health.serverOk = true;
     let asynccc = await updateHealthResponse();
@@ -32,5 +45,40 @@ async function updateHealthResponse() {
     } else {
         logger.infoWithContext('[MS-DARU-SUPPORT] - Unable to serve traffic');
         return 500
+    }
+}
+
+exports.coba = async function (req, res) {
+    try {
+        let file = req.body.file;
+        let buf = Buffer.from(file,'base64')
+        let filetype = await fileTypeFromBuffer(buf);
+        let ext = filetype.ext;
+        let mime = filetype.mime;
+    
+        let upload = await s3.listBuckets().promise();
+        // let upload = await s3.upload({
+        //     ACL: 'public-read',
+        //     Bucket: 'bucket-sit-4yrlg',
+        //     Key: `${await runNanoID(10)}.${ext}`,
+        //     Body: buf,
+        //     ContentEncoding: 'base64',
+        //     ContentType: mime,
+        // }).promise();
+        return res.status(200).json(rsMsg('000000', upload));
+    } catch (e) {
+        return res.status(500).json(errMsg('01000'));
+    }
+}
+
+exports.seatSelection = async function(req, res) {
+    try {
+        const hasil = {
+            'denah': ['A', 'B', '-', 'C', 'D']
+        }
+        return res.status(200).json(rsMsg('000000', hasil));
+    } catch (e) {
+        logger.errorWithContext({ error: e, message: 'error GET /api/v1/healthcheck/seat-selection...' });
+        return utils.returnErrorFunction(res, 'error GET /api/v1/healthcheck/seat-selection...', e);
     }
 }
--- api/v1/healthcheck/index.js
+++ api/v1/healthcheck/index.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const controller = require('./controller');
 
 router.get('/healty', controller.healtyCheck);
+router.post('/coba', controller.coba);
+
+router.get('/seat-selection', controller.seatSelection);
 
 module.exports = router;
