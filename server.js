46a63e511caef881e95c5ad40d5eef7ed06dc2b9
Muhammad Taufik <muhammad.taufik@adira.co.id>
2024-12-08 15:42:53 +0700

    update code.

--- server.js
+++ server.js
@@ -2,14 +2,12 @@
 require('dotenv').config();
 const app = require('./app');
 const logger = require('./config/logger');
-const scheduler = require('./scheduler/index').getInstance();
-// const { connectClientWS } = require('./config/websocket');
-// const { getClientSocket } = require('./config/baru');
+// const scheduler = require('./scheduler/index').getInstance();
+const { connectClientWS } = require('./config/websocket');
 // const mqttConfig = require('./config/mqtt');
 
-// connectClientWS();
-// getClientSocket();
-scheduler.getSCID();
+connectClientWS();
+// scheduler.getSCID();
 const PORT = process.env.PORT;
 const server = app.listen(PORT, () => logger.infoWithContext(`API Server started. Listening on port:${PORT}`));
 // mqttConfig.mqttForCreateLoker();
