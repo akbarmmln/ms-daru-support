014d748811b0eecc93fa6df1dd3eba469e959f25
Muhammad Taufik <muhammad.taufik@adira.co.id>
2024-12-10 15:39:24 +0700

    update code

--- config/websocket.js
+++ config/websocket.js
@@ -5,65 +5,49 @@ let reconnectDelay = 5000;
 let wsInstance = null;
 const logger = require('./logger');
 const format = require('../config/format');
-const redisClient = require('../config/redis');
 
-async function init(params) {
-    const key = `${process.env.SERVICE_NAME}-${params}`
-    const targetClient = await redisClient.hget('available_socket', `${key}`);
-    if (targetClient) {
-        const hasil = JSON.parse(targetClient);
-        clientId = hasil.socketName
-    } else {
-        clientId = null;
-    }
-}
-
-async function connectClientWS(params, podName) {
+async function connectClientWS(params, podName, clientId) {
     async function connect() {
-        if (!format.isEmpty(clientId)) {
-            wsInstance = new WebSocket(wsUrl);
+        wsInstance = new WebSocket(wsUrl);
 
-            wsInstance.on('open', () => {
-                // Register clientId
-                logger.infoWithContext(`${clientId} connected to WebSocket server`)
-                const additonal = {
-                    service: `${process.env.SERVICE_NAME}`,
-                    podsName: podName,
-                    pods: params,
-                    socketName: clientId            
-                }
-                wsInstance.send(JSON.stringify({ type: 'register', agent: 'microservice', clientId, additonal: additonal }));
-            });
-    
-            wsInstance.on('message', (message) => {
-                try {
-                    const data = JSON.parse(message);
-                    logger.infoWithContext(`Message received: ${JSON.stringify(data)}`)
-                } catch (e) {
-                    logger.errorWithContext({ error: e, message: 'Error parsing message:'})
-                }
-            });
-    
-            wsInstance.on('error', (err) => {
-                // Graceful handling: Close the connection to trigger reconnect logic
-                logger.errorWithContext({ error: err, message: `WebSocket error ${clientId}`})
-                wsInstance.close();
-            });
-    
-            wsInstance.on('close', (code, reason) => {
-                // Retry after a delay
-                logger.infoWithContext(`WebSocket connection closed. Code: ${code}, Reason: ${reason}`)
-                // logger.infoWithContext(`Retrying connection in ${reconnectDelay / 1000} seconds...`);
-                // setTimeout(connect, reconnectDelay);
-            });
-    
-            wsInstance.on('ping', () => {
-                logger.infoWithContext(`Received ping`);
-                wsInstance.pong();
-            });
-        } else {
-            logger.infoWithContext('this pod can not running as web socket client (2)')
-        }
+        wsInstance.on('open', () => {
+            // Register clientId
+            logger.infoWithContext(`${clientId} connected to WebSocket server`)
+            const additonal = {
+                service: `${process.env.SERVICE_NAME}`,
+                podsName: podName,
+                pods: params,
+                socketName: clientId            
+            }
+            wsInstance.send(JSON.stringify({ type: 'register', agent: 'microservice', clientId, additonal: additonal }));
+        });
+
+        wsInstance.on('message', (message) => {
+            try {
+                const data = JSON.parse(message);
+                logger.infoWithContext(`Message received: ${JSON.stringify(data)}`)
+            } catch (e) {
+                logger.errorWithContext({ error: e, message: 'Error parsing message:'})
+            }
+        });
+
+        wsInstance.on('error', (err) => {
+            // Graceful handling: Close the connection to trigger reconnect logic
+            logger.errorWithContext({ error: err, message: `WebSocket error ${clientId}`})
+            wsInstance.close();
+        });
+
+        wsInstance.on('close', (code, reason) => {
+            // Retry after a delay
+            logger.infoWithContext(`WebSocket connection closed. Code: ${code}, Reason: ${reason}`)
+            // logger.infoWithContext(`Retrying connection in ${reconnectDelay / 1000} seconds...`);
+            // setTimeout(connect, reconnectDelay);
+        });
+
+        wsInstance.on('ping', () => {
+            logger.infoWithContext(`Received ping`);
+            wsInstance.pong();
+        });
     }
     await init(params);
     await connect();
@@ -72,7 +56,5 @@ async function connectClientWS(params, podName) {
 
 module.exports = {
     connectClientWS,
-    getWebSocket: () => wsInstance,
     WebSocket,
-    clientId: () => clientId
 }
--- usecase/index.js
+++ usecase/index.js
@@ -3,11 +3,24 @@ const logger = require('../config/logger');
 const redisClient = require('../config/redis');
 const Constant = require('../utils/constant');
 const format = require('../config/format');
+const clients = require('../config/client');
 
 exports.connectClientSocket = async function (params, podName) {
   try {
-    const logggss = await connectClientWS(params, podName);
-    console.log('asdsadkaskdsadasd ', logggss)
+    const key = `${process.env.SERVICE_NAME}-${params}`
+    const targetClient = await redisClient.hget('available_socket', `${key}`);
+    if (targetClient) {
+      logger.infoWithContext('this pod will be running as web socket client (2)')
+      const hasil = JSON.parse(targetClient);
+      const clientId = hasil.socketName
+
+      const connectionDetails = await connectClientWS(params, podName, clientId);
+      console.log('asdsadkaskdsadasd ', connectionDetails)
+      clients.set(clientId, connectionDetails)
+    } else {
+      logger.infoWithContext('this pod can not running as web socket client (2)')
+    }
+
     // const key = `${process.env.SERVICE_NAME}-${params}`
     // const data = await redisClient.get(Constant.formatNameRedis(Constant.Constant.REDIS.WEBSOCKET_CLIENT, 'microservice', `${key}`));
     // logger.infoWithContext(`detail data client ${data}`)
