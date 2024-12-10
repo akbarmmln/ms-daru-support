e0cc31b0c3939a14f2465c46e59587410c19bc2d
Muhammad Taufik <muhammad.taufik@adira.co.id>
2024-12-10 15:11:25 +0700

    update code

--- scheduler/websocket/index.js
+++ scheduler/websocket/index.js
@@ -3,13 +3,8 @@ const connectClient = require('../../usecase/index')
 const { workerData, parentPort } = require("worker_threads");
 
 logger.infoWithContext(`Task[${workerData.topic}] Worker[${workerData.id}] start....`);
-try {
-  connectClient.connectClientSocket(workerData.part, workerData.podName)
-  // parentPort.postMessage(`Task[${workerData.topic}] Worker[${workerData.id}] part[${workerData.part}] pod[${workerData.podName}] finished....`);
-} catch (e) {
-  logger.errorWithContext({ error, message: 'Error running initiate web socket client' });
-  // parentPort.postMessage(`Task[${workerData.topic}] Worker[${workerData.id}] error....`);
-}
+connectClient.connectClientSocket(workerData.part, workerData.podName)
+
 // connectClient.connectClientSocket(workerData.part, workerData.podName)
 //   // .then(() => {
 //   //   parentPort.postMessage(`Task[${workerData.topic}] Worker[${workerData.id}] part[${workerData.part}] pod[${workerData.podName}] finished....`);
