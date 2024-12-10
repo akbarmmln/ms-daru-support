c2d49ce62bf800fe4b22b9e0b572ce66679818c0
Muhammad Taufik <muhammad.taufik@adira.co.id>
2024-12-10 14:32:10 +0700

    update code

--- scheduler/privatescheduler.js
+++ scheduler/privatescheduler.js
@@ -19,40 +19,39 @@ class PrivateScheduler {
     await this.initialize();
     const result = await this.#assignorScid.getAssignedPartition();
     if (result.state) {
-      connectClientWS(result.posititon, result.podName);
-      // const task = new Task(topic, () => {
-      //   let worker = new Worker("./scheduler/websocket/index.js",
-      //     {
-      //       workerData: {
-      //         topic: topic,
-      //         id: 1,
-      //         part: result.posititon,
-      //         podName: result.podName
-      //       }
-      //     })
-      //   worker.once("message", result => {
-      //     logger.infoWithContext(`${topic} (on message) Payload Result ${JSON.stringify(result)}`);
-      //     worker.terminate();
-      //   });
-      //   worker.once("error", result => {
-      //     logger.infoWithContext(`${topic} (on error) Payload Result ${JSON.stringify(result)}`);
-      //     worker.terminate();
-      //   });
-      //   worker.once("exit", result => {
-      //     logger.infoWithContext(`${topic} (on exit) Payload Result ${JSON.stringify(result)}`);
-      //     worker.terminate();
-      //   });
+      const task = new Task(topic, () => {
+        let worker = new Worker("./scheduler/websocket/index.js",
+          {
+            workerData: {
+              topic: topic,
+              id: 1,
+              part: result.posititon,
+              podName: result.podName
+            }
+          })
+        worker.once("message", result => {
+          logger.infoWithContext(`${topic} (on message) Payload Result ${JSON.stringify(result)}`);
+          worker.terminate();
+        });
+        worker.once("error", result => {
+          logger.infoWithContext(`${topic} (on error) Payload Result ${JSON.stringify(result)}`);
+          worker.terminate();
+        });
+        worker.once("exit", result => {
+          logger.infoWithContext(`${topic} (on exit) Payload Result ${JSON.stringify(result)}`);
+          worker.terminate();
+        });
+        scheduler.stop();
+      });
 
-      // });
+      const job = new SimpleIntervalJob(
+        {
+          // milliseconds: parseInt(86400254),
+          milliseconds: parseInt(30561),
+          runImmediately: true
+        }, task)
 
-      // const job = new SimpleIntervalJob(
-      //   {
-      //     // milliseconds: parseInt(86400254),
-      //     milliseconds: parseInt(30561),
-      //     runImmediately: true
-      //   }, task)
-
-      // scheduler.addSimpleIntervalJob(job)
+      scheduler.addSimpleIntervalJob(job)
     } else {
       logger.infoWithContext('this pod can not running as web socket client (1)')
     }
