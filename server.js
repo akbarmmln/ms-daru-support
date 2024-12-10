b5727f5da5e5f734fa5ce8e36f06b23cd83494cc
Muhammad Taufik <muhammad.taufik@adira.co.id>
2024-12-10 13:51:54 +0700

    update code

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { connectClientWS } = require('./config/websocket');
 // const mqttConfig = require('./config/mqtt');
 
 // scheduler.getSCID();
-connectClientWS();
+connectClientWS('0', 'ms-support');
 const PORT = process.env.PORT;
 const server = app.listen(PORT, () => logger.infoWithContext(`API Server started. Listening on port:${PORT}`));
 // mqttConfig.mqttForCreateLoker();
